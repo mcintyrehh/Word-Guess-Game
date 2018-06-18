f24ca0d74422cc92d88db54ae68323db26abbeb0
Onray <mcintyrehh@gmail.com>
2018-06-17 20:47:27 -0400

    everything is looking really good, just have reset button left

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,7 @@ var guessesLeft = 10;
 var wordBank = ["test", "testing", "testified", "testimonious"];
 var blankedWord = [];
 var guessedLetters = [];
+var lettersRight = 0;
 var guess;
 //*for before the game starts* printing the counter vars to their respective <p>
 document.getElementById("wins-p").innerText = wins;
@@ -25,36 +26,64 @@ document.getElementById("blanked-word").innerHTML = blankedWord.join(' ');
 document.onkeydown = function (event) {
     var guess = event.key;
     guess = guess.toLowerCase();
-    // guessedLetters.push(guess);
     console.log(guess);
     console.log(guessedLetters);
     console.log(wins);
-    //re-printing every counter with every keystroke
-    document.getElementById("wins-p").innerText = wins;
-    document.getElementById("losses").innerText = losses;
-        
+
+    // audio if they guess a letter correctly
+    var owenWow = document.createElement("audio");
+        owenWow.setAttribute("src", "Assets/24_Owen_Wilson_Wow_MP3s/wowc.mp3");
+    // for audio if they lose
+    var lossAudio = document.createElement("audio");
+        lossAudio.setAttribute("src", "Assets/HowardDean.mp3");
+    // setting up the wins/losses <div> as a var to run the rotate jQuery plugin on
+    var lossDiv = $("#lossDiv");
+    var winsDiv = $("#winsDiv");
     console.log(computerGuess.indexOf(guess) + "does it show 2?")
     // If the guessed letter is an index of the answer, a for loop looks
     // at each letter in the answer and replaces it if the guess is identical
     if (computerGuess.indexOf(guess) !== -1) {
+        owenWow.play();
         var computerGuessArray = computerGuess.split('');
         console.log(computerGuessArray);
         for (var i = 0; i < computerGuessArray.length; i++) {
             if (computerGuessArray[i] === guess) {
                 blankedWord[i] = guess;
+                lettersRight++;
                 document.getElementById("blanked-word").innerHTML = blankedWord.join(' ');
             }
         };
     }
     // If the guess isn't in the word it subtracts a guess, 
-    else if (computerGuess.indexOf(guess) === -1) {
-        guessesLeft--
+    else if ((computerGuess.indexOf(guess)) === -1 && (guessedLetters.indexOf(guess) === -1)) {
+        guessesLeft--;
         document.getElementById("guessesrem").innerText = " " + guessesLeft;
+    // If the guess hasn't already been guessed it gets added to the "letters already guessed section"
         if (guessedLetters.indexOf(guess) === -1) {
             guessedLetters.push(guess);
             document.getElementById("guessedLetters").innerHTML += guess + " ";
         }  
     }
+    if (guessesLeft === 0) {
+        lossAudio.play();
+        losses++;
+        document.getElementById("losses").innerText = losses;
+        lossDiv.rotate({
+            angle: 0,
+            animateTo: 360})
+        document.getElementById("reset").innerHTML ='<button type="button" class="btn btn-outline-light">Reset</button>';
+    }
+    if (lettersRight === computerGuess.length) {
+
+        wins++
+        document.getElementById("wins-p").innerText = wins;
+        winsDiv.rotate({
+            angle:0,
+            animateTo: 360})
+            document.getElementById("reset").innerHTML = '<button type="button" class="btn btn-outline-light">Reset</button>';
+    }
 };
+var reset = $("#reset")
+
 
 
